45e21dec4116afd4ef210f366b46f7f628b0bc36
Will <montero_w@live.com>
2021-10-09 14:27:41 -0500

    Created MiniBoss Class Interrogated Bosses Minions Function

---
+++ GameJam_Oct_21/MiniBoss.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameJam_Oct_21
+{
+    class MiniBoss : Entity
+    {
+        private int _totalMinons;
+        private string _dialog;
+
+        public int TotalMinons { get { return _totalMinons; } }
+        
+        public string Dialog { get { return _dialog; } }
+
+
+        public MiniBoss(string dialoag, int toalMinons,string name, float health, float defense,  Abilitie[] abilities):base(name, health, defense, abilities)
+        {
+            _totalMinons = toalMinons;
+            _dialog = dialoag;
+
+        }
+
+        public Entity[] BossMinions(Abilitie[] abilities)
+        {
+            Entity[] minionHord = new Entity[_totalMinons];
+            for (int i = 0; i < minionHord.Length; i++)
+                minionHord[i] = new Entity(Name + "Minion" + (i + 1), (Health / 3), (Defense * 0), abilities);
+
+            return minionHord;
+        }
+
+
+    }
+}
